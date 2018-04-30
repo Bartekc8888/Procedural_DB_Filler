62b9900cf5afc38f9c5f8dbd8fce8556fba6215a
Bartłomiej Ciesielski <bartekc8@op.pl>
2018-04-30 13:19:07 +0200

    Dodanie danych potrzebnych do generacji.

---
+++ DataStructures.py
@@ -0,0 +1,70 @@
+import datetime
+
+class City:
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+
+class Person:
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+        self.surname = ""
+        self.age = 0
+        self.salary = 0
+
+class PlayerPosition:
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+
+class Player:
+    def __init__(self):
+        self.personId = 0
+        self.playerPositionId = 0
+
+class Team:
+    def __init__(self):
+        self.id = 0
+        self.name = 0
+        self.cityId = 0
+
+class TeamPlayer:
+    def __init__(self):
+        self.teamId = 0
+        self.playerId = 0
+        self.tshirtNumber = 0
+        self.contractStartDate = datetime.date.today()
+        self.contractEndDate = datetime.date.today()
+
+class Referee:
+    def __init__(self):
+        self.personId = 0
+        self.teamId = 0
+        self.experienceInYears = 0
+        self.refereePosition = ""
+
+
+class Tournament:
+    def __init__(self):
+        self.id = 0
+        self.rank = ""
+        self.name = ""
+
+class Match:
+    def __init__(self):
+        self.id = 0
+        self.tournamentId = 0
+        self.team1Id = 0
+        self.team2Id = 0
+        self.cityId = 0
+        self.referee1Id = 0
+        self.referee2Id = 0
+        self.referee3Id = 0
+        self.date = datetime.date.today()
+
+class MatchResult:
+    def __init__(self):
+        self.matchId = 0
+        self.winnerId = 0
+        self.points = 0
---
+++ Generate.py
@@ -0,0 +1,24 @@
+import DataStructures
+
+GENERATION_DATA_DIR = "GeneratorData"
+
+CITIES_FILE_NAME = "cities.txt"
+FEMALE_NAMES_FILE_NAME = "firstFemaleNames.txt"
+MALE_NAMES_FILE_NAME = "firstMaleNames.txt"
+LAST_NAMES_FILE_NAME = "lastNames.txt" 
+PLAYER_POSITIONS_FILE_NAME = "playerPositions.txt"
+REFEREE_POSITIONS_FILE_NAME = "refereePositions.txt"
+TEAM_NAMES_FILE_NAME = "teamNames.txt"
+TOURNAMENT_NAMES_FILE_NAME = "tournamentNames.txt"
+TOURNAMENT_RANKS_FILE_NAME = "tournamentRanks.txt"
+
+NUMBER_OF_TEAMS_TO_GENERATE = 10
+
+def main():
+    t1 = DataStructures.Team()
+    t1.name = "Iskra Opole"
+
+    print(t1.name)
+
+if __name__ == "__main__":
+    main()
